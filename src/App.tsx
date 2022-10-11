5183b3cf263b2f5393f28c8fd529a91b0304b023
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-11 20:20:09 -0300

    feat: add polymorphism using Radix Slot

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { Button } from './components/button'
 import './styles/global.css'
 
 export function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="w-screen h-screen bg-zinc-900 flex items-center justify-center gap-4">
+      <Button>Login</Button>
+      <Button asChild>
+        <a href="#">Link to site</a>
+      </Button>
+    </div>
   )
 }
